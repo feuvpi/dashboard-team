ea19e75679aa69aab534c51a4ea109a0cdd23015
feuvpi <fredvpg@gmail.com>
2024-06-19 21:05:53 -0300

    api postgres

---
+++ team-manegement-api/Core/Entities/BaseModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace team_manegement_api.Core
+{
+    public class BaseModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+    }
+}
---
+++ team-manegement-api/Infrastructure/Data/DapperContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data;
+using Npgsql;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Data
+{
+    public class DapperContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public IDbConnection CreateConnection()
+            => new NpgsqlConnection(_connectionString); // Adjust based on your database provider
+    }
+}
